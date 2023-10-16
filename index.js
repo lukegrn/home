582af4cf50722fbb90458af3f66061fa095aa931
Luke Green <me@lukegreen.xyz>
2023-10-16 07:11:46 -0500

    Add support for blog posts
    This adds generation of the most recent blog posts to /blog and also
    generates all post templates in /templates/blog/posts/

--- index.js
+++ index.js
@@ -12,15 +12,40 @@ const writeTemplate = async (template, rendered) => {
   await fs.promises.writeFile(`public/${template}.html`, rendered);
 };
 
-const renderTemplates = () => {
+/*
+ * calculateMostRecentPosts returns the top 5 most recent blog entries
+ * It assumes that posts are in the templates/blog/posts directory
+ * and are named in YYYY-MM-DD format.
+ **/
+const calculateMostRecentPosts = async () => {
+  posts = (await fs.promises.readdir("./templates/blog/posts"))
+    .map((post) => post.slice(0, -4))
+    .sort((a, b) => {
+      const aDate = Date(Date.parse(`${a}T00:00:00.000Z`));
+      const bDate = Date(Date.parse(`${b}T00:00:00.000Z`));
+      return aDate < bDate ? -1 : aDate == bDate ? 0 : 1;
+    });
+
+  return posts.slice(-5);
+};
+
+const renderTemplates = async () => {
+  const blogPosts = (await fs.promises.readdir("./templates/blog/posts")).map(
+    (post) => [`blog/posts/${post.slice(0, -4)}`],
+  );
+
   const templatesToCompile = [
     // Root
     ["index"],
     ["about"],
-    ["blog"],
+    [
+      "blog",
+      {
+        posts: await calculateMostRecentPosts(),
+      },
+    ],
     // Blog pages
-    ["blog/index"],
-    ["blog/post1"],
+    ...blogPosts,
   ];
 
   templatesToCompile.forEach(([template, opts = {}]) => {
